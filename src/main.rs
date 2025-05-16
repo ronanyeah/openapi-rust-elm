eaf7d46d74ecde7b61e07e0096987aa58a23ae71
ronan <ronan_mccabe@protonmail.com>
2025-05-16 11:43:30 -0600

    Add utoipa axum_extras

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use axum::{extract::Json, http::StatusCode, routing::get};
+use axum::{
+    extract::{Json, State},
+    http::StatusCode,
+};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 use utoipa::{OpenApi, ToSchema};
 use utoipa_axum::router::OpenApiRouter;
 
@@ -6,20 +11,17 @@ use utoipa_axum::router::OpenApiRouter;
 #[openapi(info(title = "Codegen", version = "1.0.0"))]
 struct ApiDoc;
 
+#[derive(Clone)]
+struct App {
+    count: Arc<Mutex<usize>>,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let cors = tower_http::cors::CorsLayer::new()
-        .allow_methods(tower_http::cors::Any)
-        .allow_headers(tower_http::cors::Any)
-        .allow_origin(tower_http::cors::Any);
-
-    let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
-        .routes(utoipa_axum::routes!(health_handler))
-        .routes(utoipa_axum::routes!(stats_handler))
-        .layer(tower::ServiceBuilder::new().layer(cors))
-        .split_for_parts();
-
-    let router = router.route("/openapi.json", get(|| async { Json(api) }));
+    let state = App {
+        count: Arc::new(Mutex::new(0)),
+    };
+    let router = create_router(state);
 
     let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 8888));
 
@@ -28,6 +30,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     axum::serve(listener, router).await?;
 
     Ok(())
+}
+
+fn create_router(state: App) -> axum::Router<()> {
+    let cors = tower_http::cors::CorsLayer::new()
+        .allow_methods(tower_http::cors::Any)
+        .allow_headers(tower_http::cors::Any)
+        .allow_origin(tower_http::cors::Any);
+
+    let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
+        .routes(utoipa_axum::routes!(health_handler))
+        .routes(utoipa_axum::routes!(stats_handler))
+        .routes(utoipa_axum::routes!(bump_handler))
+        .routes(utoipa_axum::routes!(body_handler))
+        .layer(tower::ServiceBuilder::new().layer(cors))
+        .with_state(state)
+        .split_for_parts();
+
+    let router_with_schema =
+        router.route("/openapi.json", axum::routing::get(|| async { Json(api) }));
+
+    router_with_schema
 }
 
 #[utoipa::path(
@@ -64,4 +87,36 @@ struct Data1 {
 )]
 async fn stats_handler() -> Json<StatsData> {
     Json(Default::default())
+}
+
+#[utoipa::path(
+    get,
+    path = "/bump",
+    responses(
+        (status = 200, body = usize)
+    )
+)]
+async fn bump_handler(State(app): State<App>) -> Json<usize> {
+    let mut count = app.count.lock().await;
+    *count += 1;
+    Json(*count)
+}
+
+#[derive(serde::Deserialize, ToSchema)]
+struct Params {
+    name: String,
+    age: usize,
+}
+
+#[utoipa::path(
+    post,
+    path = "/body",
+    responses(
+        (status = 200, body = bool)
+    )
+)]
+async fn body_handler(State(_app): State<App>, Json(params): Json<Params>) -> Json<bool> {
+    let _ = params.name;
+    let _ = params.age;
+    Json(true)
 }
